6579d6c11db4cf640dd09af280d5120599b1478f
ayeshapatel03 <ayeshap3@uw.edu>
2023-03-07 12:05:12 -0800

    complete chart1 viz and widgets

--- server.R
+++ server.R
@@ -1,8 +1,45 @@
+library(plotly)
+library(bslib)
+library(stringr)
+library(dplyr)
+library(tidyverse)
+library(openintro)
+library(ggplot2)
+library(shiny)
+library(markdown)
+
+
+all_data <- read.csv("final_dataframe.csv", stringsAsFactors = FALSE)
 
 server <- function(input, output) {
   
   output$chart_1_plot <- renderPlotly({
     
+    chart_world_data <- all_data %>%
+      select(age, finalWorth, gender) %>%
+      filter(gender == "F" | gender == "M")
+    
+    chart_world_data <- chart_world_data %>%
+      na.omit(chart_world_data)
+    
+    chart_world_data <- chart_world_data %>%
+      filter(gender %in% input$gender_selection) %>%
+      filter(age > input$age_selection[1] & age < input$age_selection[2])
+    
+    chart_1_plot <- ggplot(data = chart_world_data) +
+      # geom_line(mapping = aes(x = age, 
+      #                         y = finalWorth, 
+      #                         color = gender)) +
+      geom_point(aes(x = age,
+                     y = finalWorth, 
+                    color = gender,
+                    text = paste("Age:", age, "<br>Final Worth:", finalWorth, "<br>Gender:", gender))) +
+                   scale_x_continuous(breaks = seq(20, 100, 5)) +
+                   scale_y_continuous(breaks = seq(0, 220, 25)) +
+                   labs(title = "Distribution of Age vs Net Worth by Gender", x = "Age", y = "Net Worth (in billions)", color = "Gender") +
+                   scale_color_hue(labels = c("Women", "Men"))
+                 
+      return(ggplotly(chart_1_plot, tooltip = c("text")))
   })
   
   
--- ui.R
+++ ui.R
@@ -17,61 +17,67 @@ intro_tab <- tabPanel(
   fluidPage(
     # Include a Markdown file!
     includeMarkdown("Introduction.md")
-    #p("This project analyzes trends in greenhouse gas emissions.")
   )
 )
 
 
-
 # PAGE TWO - CHART 1
-chart_1_plot <- mainPanel(
+chart_1 <- mainPanel(
   # Make plot interactive
-  plotlyOutput(outputId = "chart1")
+  plotlyOutput(outputId = "chart_1_plot")
+)
+
+age_slider_widget <- sliderInput(
+  inputId = "age_selection",
+  label = "Age",
+  min = 19,
+  max = 100,
+  value = c(20, 60),
+  sep = "")
+
+gender_checkbox_widget <- checkboxGroupInput(
+  inputId = "gender_selection",
+  label = "Gender",
+  choices = c("F", "M"),
+  selected = "F"
 )
 
 chart_1_tab <- tabPanel(
   "Data Viz 1",
   sidebarLayout(
     sidebarPanel(
-      select_widget,
-      slider_widget,
-      radio_widget),
-    chart_1_plot
+      age_slider_widget,
+      gender_checkbox_widget),
+    chart_1
   )
 )
 
 # PAGE THREE - CHART 2
-chart_2_plot <- mainPanel(
+chart_2 <- mainPanel(
   # Make plot interactive
-  plotlyOutput(outputId = "chart2")
+  plotlyOutput(outputId = "chart_2_plot")
 )
 
 chart_2_tab <- tabPanel(
   "Data Viz 2",
   sidebarLayout(
-    sidebarPanel(
-      select_widget,
-      slider_widget,
-      radio_widget),
-    chart_2_plot
+    sidebarPanel(),
+    chart_2
   )
 )
 
 
 # PAGE FOUR - CHART 3
-chart_3_plot <- mainPanel(
+chart_3 <- mainPanel(
   # Make plot interactive
-  plotlyOutput(outputId = "chart3")
+  plotlyOutput(outputId = "chart_3_plot")
 )
 
 chart_3_tab <- tabPanel(
   "Data Viz 3",
   sidebarLayout(
-    sidebarPanel(
-      select_widget,
-      slider_widget,
-      radio_widget),
-    chart_3_plot
+    sidebarPanel(),
+    chart_3
   )
 )
 
@@ -80,14 +86,14 @@ chart_3_tab <- tabPanel(
 conclusion_tab <- tabPanel(
   "Conclusion",
   fluidPage(
-    includeMarkdown()
+    #includeMarkdown()
   )
 )
 
 # COMPILE PAGE 1-5
 ui <- navbarPage(
   # Select Theme
-  theme = shinytheme("flatly"),
+  #theme = shinytheme("flatly"),
   # Home page title
   "Home Page",
   intro_tab,
